3b28d0fee08570677499ff217a581cdc535bc69b
victoralmau <victoralmau@gmail.com>
2020-07-14 08:43:31 +0200

    reestructuración

--- survey_oniad/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from . import survey_survey
-from . import survey_mail_compose_message
-from . import survey_user_input 
