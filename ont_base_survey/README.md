1fccc012321e7e8290360c5084e5cad845cee0c3
victoralmau <victoralmau@gmail.com>
2020-07-14 07:44:56 +0200

    reestructuración

--- ont_base_survey/README.md
+++
@@ -1,1 +0,0 @@
-El módulo añade los campos de Nº de intentos y Fecha siguiente intento respecto a las respuestas de las encuestas.
--- survey_oniad/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-import survey_survey
-import survey_mail_compose_message
-import survey_user_input
