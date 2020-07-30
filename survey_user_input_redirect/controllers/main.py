f419b141378d20d98461f69edf2b6251ea8c9c11
victoralmau <victoralmau@gmail.com>
2020-07-30 08:21:40 +0200

    errors

--- survey_user_input_redirect/controllers/main.py
+++ survey_user_input_redirect/controllers/main.py
@@ -1,5 +1,5 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import http
+import odoo
 from odoo.http import request
 import odoo.addons.survey.controllers.main as main
 
